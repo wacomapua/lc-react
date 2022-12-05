25aa0c7a9a1b9e5211684779826b5c3abd787dc9
Waco Mapua <110009518+wacomapua@users.noreply.github.com>
2022-12-05 23:13:01 +1000

    initial markup and css

--- src/Another.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Another(props) {
-    return (
-        <div>
-            Another Component, {props.name}
-        </div>
-    )
-}
--- src/components/AppOriginal.jsx
+++ src/components/AppOriginal.jsx
@@ -1,8 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
 import { useState } from 'react';
-import React from 'react';
 import Another from './Another';
+import logo from './logo.svg';
+import '../App.css';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -18,18 +17,16 @@ function App() {
   const someStyle = {
     background: 'blue',
     color: 'white',
-    fontSize: '40px',
+    fontSize: '28px',
     fontWeight: 'bold',
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <Another name="Waco" />
-
+        <Another name="Andre" />
         <div>
           <span>{count}</span>
-
           <button onClick={decrement}>-</button>
           <button onClick={increment}>+</button>
         </div>
@@ -37,7 +34,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p style={someStyle}>{3 + 2}</p>
+        {true && <p style={someStyle}>{3 + 2}</p>}
         <a
           className="App-link"
           href="https://reactjs.org"
