e6b940ffddd456d806f4b3dd0bc29ce294828b4f
Waco Mapua <110009518+wacomapua@users.noreply.github.com>
2022-12-05 22:00:44 +1000

    Episode 1

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import React from "react";
+import Another from "./Another";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function decrement() {
+    setCount((prevCount) => prevCount - 1);
+  }
+
+  function increment() {
+    setCount((prevCount) => prevCount + 1);
+  }
+
+  const someStyle = {
+    background: "blue",
+    color: "white",
+    fontSize: "40px",
+    fontWeight: "bold",
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Another name="Waco" />
+        
+        <div>
+          <span>{count}</span>
+
+          <button onClick={decrement}>-</button>
+          <button onClick={increment}>+</button>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p style={someStyle}>{3 + 2}</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
